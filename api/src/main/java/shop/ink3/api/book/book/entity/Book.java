01d9d74934e1b495a92d5bb78181c09379d1c748
Jihyun3478 <leejihyun3478@gmail.com>
2025-05-12 17:59:57 +0900

    modify: cart 양방향 매핑 제거

--- api/src/main/java/shop/ink3/api/book/book/entity/Book.java
+++ api/src/main/java/shop/ink3/api/book/book/entity/Book.java
@@ -77,9 +77,6 @@ public class Book {
     @Column(nullable = false)
     private String thumbnailUrl;
 
-    @OneToMany(mappedBy = "book")
-    private List<Cart> carts = new ArrayList<>();
-
     @OneToMany(mappedBy = "book",
             cascade = CascadeType.ALL,
             orphanRemoval = true)
