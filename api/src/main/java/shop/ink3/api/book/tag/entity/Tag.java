9ec53e8a3b1e9d40612d18c2d9e7b0989ba2a422
messier333 <bdqueen3250@gmail.com>
2025-05-13 14:08:52 +0900

    feat:entity

--- api/src/main/java/shop/ink3/api/book/tag/entity/Tag.java
+++ api/src/main/java/shop/ink3/api/book/tag/entity/Tag.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
@@ -16,7 +17,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-import shop.ink3.api.book.bookAuthor.entity.BookAuthor;
 import shop.ink3.api.book.bookTag.entity.BookTag;
 
 @Entity
@@ -35,12 +35,16 @@ public class Tag {
     private String name;
 
     @Builder.Default
-    @OneToMany(mappedBy = "tags",
+    @OneToMany(mappedBy = "tag",
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private List<BookTag> bookTags = new ArrayList<>();
 
     public void addBookTag(BookTag bookTag) {
         this.bookTags.add(bookTag);
+    }
+
+    public void updateTagName(@NotBlank String name) {
+        this.name = name;
     }
 }
