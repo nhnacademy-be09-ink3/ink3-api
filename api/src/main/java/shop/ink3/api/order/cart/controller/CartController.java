75e274a68c2750023a59bf47fda29e4149a2cb87
Jihyun3478 <leejihyun3478@gmail.com>
2025-05-15 15:21:52 +0900

    hotfix: HTTP Method 수정

--- api/src/main/java/shop/ink3/api/order/cart/controller/CartController.java
+++ api/src/main/java/shop/ink3/api/order/cart/controller/CartController.java
@@ -2,11 +2,13 @@ package shop.ink3.api.order.cart.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,7 +31,7 @@ public class CartController {
         return ResponseEntity.ok(cartService.addCartItem(request));
     }
 
-    @GetMapping("/{cartId}")
+    @PutMapping("/{cartId}")
     public ResponseEntity<CartResponse> updateQuantity(@PathVariable Long cartId, @RequestBody CartUpdateRequest request) {
         return ResponseEntity.ok(cartService.updateCartQuantity(cartId, request));
     }
