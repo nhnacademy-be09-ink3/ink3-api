dad0cbdf01026b6e0c4085117acf83d812ecf32b
snackcookie <fghkk12d@gmail.com>
2025-05-22 14:35:12 +0900

    feat: yml, pom 수정

--- api/src/main/java/shop/ink3/api/common/config/RabbitTemplateConfig.java
+++ api/src/main/java/shop/ink3/api/common/config/RabbitTemplateConfig.java
@@ -1,5 +1,6 @@
 package shop.ink3.api.common.config;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +9,8 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitTemplateConfig {
     @Bean
     @ConditionalOnProperty(name = "rabbit.enabled", havingValue = "true", matchIfMissing = true)
-    public RabbitTemplate rabbitTemplate() {
-        return new RabbitTemplate();
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
     }
 }
 
