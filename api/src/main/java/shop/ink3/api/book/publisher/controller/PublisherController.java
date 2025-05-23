78dc10c668ca8317816e9d4e7ca8bb51a66398f2
junopo <junopo@naver.com>
2025-05-23 14:11:38 +0900

    hotfix: 누락된 import문 추가

--- api/src/main/java/shop/ink3/api/book/publisher/controller/PublisherController.java
+++ api/src/main/java/shop/ink3/api/book/publisher/controller/PublisherController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shop.ink3.api.book.publisher.dto.PublisherCreateRequest;
 import shop.ink3.api.book.publisher.dto.PublisherResponse;
