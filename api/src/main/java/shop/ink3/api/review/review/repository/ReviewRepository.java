c48176099a1565cf1c44ac8d6b0ae82e1a610007
Jihyun3478 <leejihyun3478@gmail.com>
2025-05-29 09:58:24 +0900

    hotfix: DTO Projection으로 N+1 문제 해결

--- api/src/main/java/shop/ink3/api/review/review/repository/ReviewRepository.java
+++ api/src/main/java/shop/ink3/api/review/review/repository/ReviewRepository.java
@@ -6,11 +6,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import shop.ink3.api.review.review.dto.ReviewListResponse;
 import shop.ink3.api.review.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Review findReviewByUserId(Long userId);
 
-    @Query("SELECT r FROM Review r WHERE r.orderBook.book.id = :bookId")
-    Page<Review> findAllByBookId(Pageable pageable, @Param("bookId") Long bookId);
+    @Query("""
+            SELECT new shop.ink3.api.review.review.dto.ReviewListResponse(
+                r.id,
+                u.id,
+                ob.id,
+                u.name,
+                r.title,
+                r.content,
+                r.rating,
+                r.createdAt,
+                r.modifiedAt
+            )
+            FROM Review r
+            JOIN r.user u
+            JOIN r.orderBook ob
+            JOIN ob.book b
+            WHERE b.id = :bookId
+        """)
+    Page<ReviewListResponse> findListByBookId(Pageable pageable, @Param("bookId") Long bookId);
 }
