2c0b91118a9fad576f7c5ed180a5612ae6706b92
messier333 <bdqueen3250@gmail.com>
2025-05-29 17:45:12 +0900

    feat:update test yml

--- api/src/main/java/shop/ink3/api/common/config/RabbitConfig.java
+++ api/src/main/java/shop/ink3/api/common/config/RabbitConfig.java
@@ -5,7 +5,11 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -102,6 +106,22 @@ public class RabbitConfig {
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter
+    ) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 }
 
--- api/src/main/java/shop/ink3/api/coupon/rabbitMq/message/WelcomeCouponMessage.java
+++ api/src/main/java/shop/ink3/api/coupon/rabbitMq/message/WelcomeCouponMessage.java
@@ -1,5 +1,6 @@
 package shop.ink3.api.coupon.rabbitMq.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,5 +9,5 @@ import org.springframework.stereotype.Component;
 import shop.ink3.api.coupon.coupon.service.Impl.CouponServiceImpl;
 import shop.ink3.api.coupon.store.service.CouponStoreService;
 
-public record WelcomeCouponMessage(Long userId) {
+public record WelcomeCouponMessage(@JsonProperty("userId") Long userId) {
 }
