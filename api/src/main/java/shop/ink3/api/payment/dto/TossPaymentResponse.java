12e4511e4cf6378ebf0aef4091c2682d80d227f1
권용민 <128355951+moooooooonlight@users.noreply.github.com>
2025-05-30 09:12:42 +0900

    hotfix: tossResponse는 LocalDateTime불가->OffsetDateTime으로 변경

--- api/src/main/java/shop/ink3/api/payment/dto/TossPaymentResponse.java
+++ api/src/main/java/shop/ink3/api/payment/dto/TossPaymentResponse.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public record TossPaymentResponse (
         String paymentKey,
         int totalAmount,
-        LocalDateTime requestedAt,
-        LocalDateTime approvedAt
+        OffsetDateTime requestedAt,
+        OffsetDateTime  approvedAt
 ){
 }
