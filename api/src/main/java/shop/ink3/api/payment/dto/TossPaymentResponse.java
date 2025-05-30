6ef456b6b52c615be26aa9265c05309cc21da438
권용민 <kwonym0914@gmail.com>
2025-05-30 13:26:56 +0900

    hotfix: tossResponse에 JsonIngoreProperty설정으로 예상 외 필드 무시

--- api/src/main/java/shop/ink3/api/payment/dto/TossPaymentResponse.java
+++ api/src/main/java/shop/ink3/api/payment/dto/TossPaymentResponse.java
@@ -1,7 +1,9 @@
 package shop.ink3.api.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.OffsetDateTime;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record TossPaymentResponse (
         String paymentKey,
         int totalAmount,
