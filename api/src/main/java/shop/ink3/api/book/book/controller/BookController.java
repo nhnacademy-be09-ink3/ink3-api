0390e83c5f3f6ab07e490973ae89ddd08d29805a
Jihyun3478 <leejihyun3478@gmail.com>
2025-06-02 12:57:54 +0900

    refactor: 공통된 API 경로 @RequestMapping으로 묶기

--- api/src/main/java/shop/ink3/api/book/book/controller/BookController.java
+++ api/src/main/java/shop/ink3/api/book/book/controller/BookController.java
@@ -1,9 +1,5 @@
 package shop.ink3.api.book.book.controller;
 
-import java.util.List;
-
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import shop.ink3.api.book.book.dto.BookCreateRequest;
 import shop.ink3.api.book.book.dto.BookResponse;
 import shop.ink3.api.book.book.dto.BookUpdateRequest;
@@ -22,65 +22,66 @@ import shop.ink3.api.book.book.service.BookService;
 import shop.ink3.api.common.dto.CommonResponse;
 import shop.ink3.api.common.dto.PageResponse;
 
+@RequestMapping("/books")
 @RequiredArgsConstructor
 @RestController
 public class BookController {
     private final BookService bookService;
 
     // 도서 상세 조회 (ID 기반 단건 조회)
-    @GetMapping("/books/{bookId}")
+    @GetMapping("/{bookId}")
     public ResponseEntity<CommonResponse<BookResponse>> getBookById(@PathVariable Long bookId) {
         return ResponseEntity.ok(CommonResponse.success(bookService.getBook(bookId)));
     }
 
      // 전체 도서 목록 조회
-    @GetMapping("/books")
+    @GetMapping
     public ResponseEntity<CommonResponse<PageResponse<BookResponse>>> getBooks(Pageable pageable) {
         return ResponseEntity.ok(CommonResponse.success(bookService.getBooks(pageable)));
     }
 
-    @GetMapping("/books/bestseller")
+    @GetMapping("/bestseller")
     public ResponseEntity<CommonResponse<PageResponse<MainBookResponse>>> getTop5BestsellerBooks() {
         return ResponseEntity.ok(CommonResponse.success(bookService.getTop5BestSellerBooks()));
     }
 
-    @GetMapping("/books/bestseller-all")
+    @GetMapping("/bestseller-all")
     public ResponseEntity<CommonResponse<PageResponse<MainBookResponse>>> getAllBestsellerBooks(Pageable pageable) {
         return ResponseEntity.ok(CommonResponse.success(bookService.getAllBestSellerBooks(pageable)));
     }
 
-    @GetMapping("/books/new")
+    @GetMapping("/new")
     public ResponseEntity<CommonResponse<PageResponse<MainBookResponse>>> getTop5NewBooks() {
         return ResponseEntity.ok(CommonResponse.success(bookService.getTop5NewBooks()));
     }
 
-    @GetMapping("/books/new-all")
+    @GetMapping("/new-all")
     public ResponseEntity<CommonResponse<PageResponse<MainBookResponse>>> getAllNewBooks(Pageable pageable) {
         return ResponseEntity.ok(CommonResponse.success(bookService.getAllNewBooks(pageable)));
     }
 
-    @GetMapping("/books/recommend")
+    @GetMapping("/recommend")
     public ResponseEntity<CommonResponse<PageResponse<MainBookResponse>>> getTop5RecommendedBooks() {
         return ResponseEntity.ok(CommonResponse.success(bookService.getTop5RecommendedBooks()));
     }
 
-    @GetMapping("/books/recommend-all")
+    @GetMapping("/recommend-all")
     public ResponseEntity<CommonResponse<PageResponse<MainBookResponse>>> getAllRecommendedBooks(Pageable pageable) {
         return ResponseEntity.ok(CommonResponse.success(bookService.getAllRecommendedBooks(pageable)));
     }
 
-    @PostMapping("/books")
+    @PostMapping
     public ResponseEntity<CommonResponse<BookResponse>> createBook(@RequestBody @Valid BookCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.create(bookService.createBook(request)));
     }
 
-    @PutMapping("/books/{bookId}")
+    @PutMapping("/{bookId}")
     public ResponseEntity<CommonResponse<BookResponse>> updateBook(@PathVariable Long bookId,
                                                                    @RequestBody @Valid BookUpdateRequest request) {
         return ResponseEntity.ok(CommonResponse.update(bookService.updateBook(bookId, request)));
     }
 
-    @DeleteMapping("/books/{bookId}")
+    @DeleteMapping("/{bookId}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long bookId) {
         bookService.deleteBook(bookId);
         return ResponseEntity.noContent().build();
