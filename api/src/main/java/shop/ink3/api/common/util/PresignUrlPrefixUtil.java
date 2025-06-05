18fe95277f43d9602d2b946f499d47be0a534c7a
Jihyun3478 <leejihyun3478@gmail.com>
2025-06-05 09:39:59 +0900

    hotfix: dev 환경에서는 dev가 붙도록 수정

--- api/src/main/java/shop/ink3/api/common/util/PresignUrlPrefixUtil.java
+++ api/src/main/java/shop/ink3/api/common/util/PresignUrlPrefixUtil.java
@@ -14,13 +14,13 @@ import lombok.extern.slf4j.Slf4j;
  * -> 이유 : 현재 http로 접근하는 url은 웹에서 보안 정책으로 인해 https로 업그레이드 하고 있음, 이 과정에서 오류 발생
  * -> 외부 API에서 가져온 이미지는 https로 시작할 것이기 때문에 해당 util 적용/미적용 뷴기가 필요함
  */
-@Slf4j
 @Component
+@Slf4j
 public class PresignUrlPrefixUtil {
+
     private final Environment environment;
 
     private static final String PREFIX = "http://storage.java21.net:8000/";
-    private static final String PROXY_PREFIX = "/image-proxy/";
 
     public PresignUrlPrefixUtil(Environment environment) {
         this.environment = environment;
@@ -32,10 +32,15 @@ public class PresignUrlPrefixUtil {
         }
 
         List<String> profiles = Arrays.asList(environment.getActiveProfiles());
-        if ((profiles.contains("dev") || profiles.contains("prod")) && presignedUrl.startsWith(PREFIX)) {
-            return PROXY_PREFIX + presignedUrl.substring(PREFIX.length());
+
+        if (profiles.contains("dev") && presignedUrl.startsWith(PREFIX)) {
+            return "/dev/image-proxy/" + presignedUrl.substring(PREFIX.length());
         }
-        log.warn("Active profile from system property: {}", System.getProperty("spring.profiles.active"));
+
+        if (profiles.contains("prod") && presignedUrl.startsWith(PREFIX)) {
+            return "/image-proxy/" + presignedUrl.substring(PREFIX.length());
+        }
+
         return presignedUrl;
     }
 }
