dd2edcef00151245f84a26af45223107256e4261
KastanEr <skuller320@naver.com>
2025-06-05 13:00:58 +0900

    hotfix: dto 카멜케이스로 수정

--- api/src/main/java/shop/ink3/api/user/point/policy/dto/PointPolicyCreateRequest.java
+++ api/src/main/java/shop/ink3/api/user/point/policy/dto/PointPolicyCreateRequest.java
@@ -2,8 +2,8 @@ package shop.ink3.api.user.point.policy.dto;
 
 public record PointPolicyCreateRequest(
         String name,
-        Integer join_point,
-        Integer review_point,
-        Integer default_rate
+        Integer joinPoint,
+        Integer reviewPoint,
+        Integer defaultRate
 ) {
 }
