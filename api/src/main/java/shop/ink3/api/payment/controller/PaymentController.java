abc3233c0b1c1a16c02ca0ca80a32a9580ff1da8
권용민 <kwonym0914@gmail.com>
2025-06-08 14:00:36 +0900

    hotfix: 0원 결제시 로직 추가

--- api/src/main/java/shop/ink3/api/payment/controller/PaymentController.java
+++ api/src/main/java/shop/ink3/api/payment/controller/PaymentController.java
@@ -12,8 +12,13 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.ink3.api.common.dto.CommonResponse;
+import shop.ink3.api.order.order.dto.OrderStatusUpdateRequest;
+import shop.ink3.api.order.order.entity.OrderStatus;
+import shop.ink3.api.order.order.service.OrderService;
 import shop.ink3.api.payment.dto.PaymentConfirmRequest;
 import shop.ink3.api.payment.dto.PaymentResponse;
+import shop.ink3.api.payment.dto.ZeroPaymentRequest;
+import shop.ink3.api.payment.entity.PaymentType;
 import shop.ink3.api.payment.service.PaymentService;
 
 @Slf4j
@@ -22,16 +27,26 @@ import shop.ink3.api.payment.service.PaymentService;
 @RequestMapping("/payments")
 public class PaymentController {
     private final PaymentService paymentService;
+    private final OrderService orderService;
 
     // 결제 승인 API 호출 및 결과 저장
     @PostMapping("/confirm")
     public ResponseEntity<CommonResponse<PaymentResponse>> confirmPayment(
-            @RequestBody PaymentConfirmRequest confirmRequest
-    ) {
+            @RequestBody PaymentConfirmRequest confirmRequest) {
         log.info("payType={}", confirmRequest.paymentType());
         String paymentApproveResponse = paymentService.callPaymentAPI(confirmRequest);
         PaymentResponse paymentResponse = paymentService.createPayment(confirmRequest, paymentApproveResponse);
         return ResponseEntity.ok(CommonResponse.success(paymentResponse));
+    }
+
+    // 결제 생성 (0원 결제 시 사용)
+    @PostMapping
+    public ResponseEntity<CommonResponse<PaymentResponse>> createPayment(
+            @RequestBody ZeroPaymentRequest zeroPaymentRequest) {
+        log.info("payType={}", PaymentType.POINT);
+        PaymentResponse zeroPayment = paymentService.createZeroPayment(zeroPaymentRequest);
+        orderService.updateOrderStatus(zeroPayment.orderId(), new OrderStatusUpdateRequest(OrderStatus.CONFIRMED));
+        return ResponseEntity.ok(CommonResponse.success(zeroPayment));
     }
 
     // 결제 실패 처리 (회원)
