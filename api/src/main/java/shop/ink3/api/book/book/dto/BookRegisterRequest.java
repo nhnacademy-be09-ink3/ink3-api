69d46a1677c7b335c83fb1732189f5cd3b33f698
junopo <junopo@naver.com>
2025-06-14 21:50:25 +0900

    hotfix: 알라딘 도서 등록 시 카테고리 연관관계 설정 오류 수정, 관리자 도서 등록/수정 기능 정상화

--- api/src/main/java/shop/ink3/api/book/book/dto/BookRegisterRequest.java
+++ api/src/main/java/shop/ink3/api/book/book/dto/BookRegisterRequest.java
@@ -1,5 +1,7 @@
 package shop.ink3.api.book.book.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.util.List;
 import shop.ink3.api.book.book.entity.BookStatus;
 import shop.ink3.api.book.book.external.aladin.dto.AladinBookResponse;
@@ -7,9 +9,9 @@ import shop.ink3.api.book.book.external.aladin.dto.AladinBookResponse;
 public record BookRegisterRequest(
         AladinBookResponse aladinBookResponse,
         String contents,
-        Integer priceSales,
-        Integer quantity,
-        BookStatus status,
+        @NotNull @PositiveOrZero Integer priceSales,
+        @NotNull @PositiveOrZero Integer quantity,
+        @NotNull BookStatus status,
         Boolean isPackable,
         List<String> tags
 ) {
